e8db86087f17bcb076f0d7be8e0db0266d2dc07e
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-07-02 17:26:06 -0400

    Updated UI, sounds, and functionality.

--- scripts/tip.js
+++ scripts/tip.js
@@ -1,21 +1,43 @@
-
-const calcTipButton = document.querySelector("#calculate-tip");
+const billInput = document.getElementById("bill");
 const tipDiv = document.querySelector("#tip");
 const totalBillDiv = document.querySelector("#total-bill");
+const tipSlider = document.querySelector("#tip-slider");
+const tipPercentage = document.querySelector("#tip-percentage");
+const calcTipButton = document.querySelector("#calculate-tip");
+const clickSound = document.querySelector("#click-sound");
+let tipPercent = 20;
+
+billInput.addEventListener("change", function() {
+    let v = parseFloat(this.value);
+    if (isNaN(v)) {
+        this.value = '';
+    } else {
+        this.value = v.toFixed(2);
+    }
+});
+
+tipSlider.addEventListener("mousemove", function(event) {
+    tipPercentage.innerHTML = `Tip Percentage: ${event.target.value}%`;
+    tipPercent = event.target.value;
+});
 
 calcTipButton.addEventListener("click", function() {
-    let billAmount = document.getElementById("bill").value;
+    let billAmount = billInput.value;
     let tip = calculateTip(billAmount);
     let total = (Number(billAmount) + Number(tip));
     tip = "$" + tip;
     total = "$" + Number(total).toFixed(2);
 
+    clickSound.src = "/sounds/click.mp3";
+    clickSound.play();
+
     tipDiv.innerHTML = `Tip: ${addCommas(tip)}`;
     totalBillDiv.innerHTML = `Total Bill: ${addCommas(total)}`;
-})
+
+});
 
 function calculateTip(billAmount) {
-    let tip = (Number(billAmount) * .20).toFixed(2);
+    let tip = (Number(billAmount) * Number(tipPercent / 100)).toFixed(2);
     return tip;
 }
 
